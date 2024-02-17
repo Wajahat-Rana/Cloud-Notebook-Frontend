dfb95387f6fdaff723b2a6b4ae56cad8cb98eef9
Rana Wajahat Saleem <128230044+Wajahat-Rana@users.noreply.github.com>
2024-02-17 11:58:02 +0500

    Added Delete+Edit Icons and Alert

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import {BrowserRouter,Routes,Route}from 'react-router-dom'
 import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 
 function App() {
   return (
     <NoteState>
     <BrowserRouter>
     <Navbar />
+    <Alert />
     <Routes>
     <Route exact path='/about' element={<About />}></Route>
     <Route exact path='/' element={<Home />}></Route>
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -8,6 +8,8 @@ const NoteItem = (props) => {
             <div class="card-body">
                 <h5 class="card-title">{note.title}</h5>
                 <p class="card-text">{note.description}</p>
+                <i class="fa-solid fa-trash mx-1"></i>
+                <i class="fa-solid fa-pen-to-square mx-3"></i>
                 {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
             </div>
         </div>
